25aeecbd512ab118b02b5f75907355eb540038c5
A-Bong <tychejin@naver.com>
2022-10-30 10:46:08 +0900

    Feat: [Spring Boot] Kotlin으로 REST API 만들기(5) - Mapper 구현 및 단위 테스트(Junit5)

--- src/main/kotlin/com/example/springbootrestapi/mapper/TodoMapper.kt
+++ src/main/kotlin/com/example/springbootrestapi/mapper/TodoMapper.kt
@@ -7,6 +7,18 @@ import org.springframework.stereotype.Repository
 @Repository
 interface TodoMapper {
 
-  /** To-Do 조회 */
+  /** To-Do 목록 조회 */
   fun getTodos(todoRequest: TodoRequest): MutableList<TodoResponse>
+
+  /** To-Do 상세 조회 */
+  fun getTodoById(id: Long): TodoResponse
+
+  /** To-Do 저장 */
+  fun insertTodo(todoRequest: TodoRequest): Int
+
+  /** To-Do 수정 */
+  fun updateTodo(todoRequest: TodoRequest): Int
+
+  /** To-Do 삭제 */
+  fun deleteTodoById(id: Long): Int
 }
---
+++ src/test/kotlin/com/example/springbootrestapi/mapper/TodoMapperTest.kt
@@ -0,0 +1,145 @@
+package com.example.springbootrestapi.mapper
+
+import com.example.springbootrestapi.domain.TodoRequest
+import com.example.springbootrestapi.domain.TodoResponse
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.transaction.annotation.Transactional
+
+@SpringBootTest
+@ActiveProfiles("local")
+class TodoMapperTest {
+
+  @Autowired
+  lateinit var todoMapper: TodoMapper
+
+  @Transactional
+  @DisplayName("getTodos_To-Do 목록 조회")
+  @Test
+  fun testGetTodos() {
+
+    // Given
+    insertTodo("Title Junit Test Insert 01", "Description Junit Test Insert 01", false)
+    insertTodo("Title Junit Test Insert 02", "Description Junit Test Insert 02", true)
+    insertTodo("Title Junit Test Insert 03", "Description Junit Test Insert 03", false)
+    insertTodo("Title Junit Test Insert 04", "Description Junit Test Insert 04", true)
+    insertTodo("Title Junit Test Insert 05", "Description Junit Test Insert 05", false)
+
+    var todoRequest = TodoRequest().apply {
+      this.title = "Title Junit Test Insert"
+      this.description = "Description Junit Test Insert"
+      this.completed = true
+    }
+
+    // When
+    var todos: MutableList<TodoResponse> = todoMapper.getTodos(todoRequest)
+
+    // Then
+    Assertions.assertTrue(!todos.isEmpty())
+  }
+
+  @Transactional
+  @DisplayName("getTodoById_To-Do 상세 조회")
+  @Test
+  fun testGetTodoById() {
+
+    // Given
+    var insertId: Long =
+      insertTodo("Title Junit Test Insert 01", "Description Junit Test Insert 01", false)
+
+    // When
+    var todoResponse: TodoResponse = todoMapper.getTodoById(insertId)
+
+    // Then
+    Assertions.assertEquals(insertId, todoResponse.id)
+  }
+
+  @Transactional
+  @DisplayName("insertTodo_To-Do 저장")
+  @Test
+  fun testInsertTodo() {
+
+    // Given
+    var todoRequest: TodoRequest = TodoRequest().apply {
+      this.title = "Title Junit Test Insert"
+      this.description = "Description Junit Test Insert"
+      this.completed = false
+    }
+
+    // When
+    todoMapper.insertTodo(todoRequest)
+
+    // Then
+    todoRequest.id?.let {
+      var todoResponse: TodoResponse = todoMapper.getTodoById(it)
+      Assertions.assertEquals(todoRequest.title, todoResponse.title)
+      Assertions.assertEquals(todoRequest.description, todoResponse.description)
+      Assertions.assertEquals(todoRequest.completed, todoResponse.completed)
+    } ?: throw Exception()
+  }
+
+  @Transactional
+  @DisplayName("updateTodo_To-Do 수정")
+  @Test
+  fun testUpdateTodo() {
+
+    // Given
+    val insertId = insertTodo("Title Junit Test Insert", "Description Junit Test Insert", false)
+
+    val todoRequest: TodoRequest = TodoRequest().apply {
+      this.id = insertId
+      this.title = "Title Junit Test Update"
+      this.description = "Description Junit Test Update"
+      this.completed = true
+    }
+
+    // When
+    todoMapper.updateTodo(todoRequest)
+
+    // Then
+    todoRequest.id?.let {
+      var todoResponse: TodoResponse = todoMapper.getTodoById(it)
+      Assertions.assertEquals(todoRequest.title, todoResponse.title)
+      Assertions.assertEquals(todoRequest.description, todoResponse.description)
+      Assertions.assertEquals(todoRequest.completed, todoResponse.completed)
+    } ?: throw Exception()
+  }
+
+  @Transactional
+  @DisplayName("deleteTodoById_To-Do 삭제")
+  @Test
+  fun testDeleteTodoById() {
+
+    // Given
+    val insertId =
+      insertTodo("Title Junit Test Insert", "Description Junit Test Insert", false)
+
+    // When
+    todoMapper.deleteTodoById(insertId)
+
+    // Then
+    val todoResponse: TodoResponse = todoMapper.getTodoById(insertId)
+    Assertions.assertNull(todoResponse)
+  }
+
+  fun insertTodo(
+    title: String,
+    description: String,
+    completed: Boolean
+  ): Long {
+
+    var todoRequest = TodoRequest().apply {
+      this.title = title
+      this.description = description
+      this.completed = completed
+    }
+
+    todoMapper.insertTodo(todoRequest)
+
+    return todoRequest.id ?: 0
+  }
+}
